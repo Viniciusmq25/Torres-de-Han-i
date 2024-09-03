0b1e33179a551c1831b2aa880d584ec3fe6b71b1
Viniciusmq25 <viniciusquintian@gmail.com>
2024-09-03 19:26:09 -0300

    Manual pronto mas sem o MINHA PILHA

--- Manual.java
+++ Manual.java
@@ -4,82 +4,108 @@ public class Manual {
   public static void modoManual() {
     Scanner inp = new Scanner(System.in);
 
+    System.out.println("quantos números voce deseja usar? (quanto mais números, mais demorado!)");
+    int qntDiscos = inp.nextInt();
 
+    int [] []torres = new int[qntDiscos+1] [3];
+    int [] []possivelResultado1 = new int[qntDiscos+1] [3];
+    int [] []possivelResultado2 = new int[qntDiscos+1] [3];
+
+    Torres.pulaLinha();
+    System.out.println("Sua piramide tem " + qntDiscos + " números!");
+
+    int cont = 0;
+
+    //inicializando as torres
+    for (int i = 0; i < torres.length; i++) {
+        torres[i][0] = cont;
+        possivelResultado1[i][1] = cont;
+        possivelResultado2[i][2] = cont;
+        cont++;
+    }
+
+    Torres.MostraMatriz(torres);
+    Torres.pulaLinha();
+
+    int value = 0;
+    int jogadas = 0;
     do {
-      System.out.println("você pode desistir a qualquer momento digitando 10");
-      System.out.println("digite o valor que deseja mover: ");
+        System.out.println("você pode desistir a qualquer momento digitando -1");
+        System.out.println("digite o valor que deseja mover: ");
 
-      int numeroPMover = inp.nextInt();
-      value = numeroPMover;
+        int numeroPMover = inp.nextInt();
+        value = numeroPMover;
 
-      //verifica se o valor digitado é zero
-      if (numeroPMover == 0){
-        divisor();
-        System.out.println("O valor zero indica que o espaço está vazio!");
-        divisor();
-      }
-      int [] localizacaoDoNum = localizaPosicao(torres, numeroPMover);
-      
-      //verifica se o valor digitado existe na torre
-      if(localizacaoDoNum == null) {
-        divisor();
-        System.out.println("O numero digitado não existe na torre!");
-        divisor();
-      }
-      else if(torres[localizacaoDoNum[0]-1][localizacaoDoNum[1]] > 0) {
-        divisor();
-        System.out.println("você só pode mover o numero do topo de uma coluna! ");
-        divisor();
-      }
-      else {
-        System.out.println("digite a coluna para onde deseja mover o numero");
-        int coluna = inp.nextInt();
-        value = coluna;
-        //verifica se a coluna existe
-        if (coluna > 3 || coluna < 1) {
-          divisor();
-          System.out.println("numero invalido, você só tem 3 colunas");
-          divisor();
-        } else{
-          int linha = qntDiscos;
+        //verifica se o valor digitado é zero
+        if (numeroPMover == 0){
+          Torres.divisor();
+          System.out.println("O valor zero indica que o espaço está vazio!");
+          Torres.divisor();
+        }
+        int [] localizacaoDoNum = Torres.localizaPosicao(torres, numeroPMover);
+        
+        //verifica se o valor digitado existe na torre
+        if(localizacaoDoNum == null && value != -1) {
+          Torres.divisor();
+          System.out.println("O numero digitado não existe na torre!");
+          Torres.divisor();
+        }
+        else if(torres[localizacaoDoNum[0]-1][localizacaoDoNum[1]] > 0) {
+          Torres.divisor();
+          System.out.println("você só pode mover o numero do topo de uma coluna! ");
+          Torres.divisor();
+        }
+        else {
+          System.out.println("digite a coluna para onde deseja mover o numero");
+          int coluna = inp.nextInt();
+          value = coluna;
+          //verifica se a coluna existe
+          if (coluna > 3 || coluna < 1) {
+            Torres.divisor();
+            System.out.println("numero invalido, você só tem 3 colunas");
+            Torres.divisor();
+          } else{
+            int linha = qntDiscos;
 
-          //verifica se o numero a ser movido pode ser colocado na torre
-          if (torres[linha][coluna-1] != 0 ){
-            while (torres[linha][coluna-1] != 0 ) {
-              linha--;
-            }
+            //verifica se o numero a ser movido pode ser colocado na torre
+            if (torres[linha][coluna-1] != 0 ){
+              while (torres[linha][coluna-1] != 0 ) {
+                linha--;
+              }
 
-            //verifica se o numero pode ser colocado
-            if(torres[linha+1][coluna-1] > numeroPMover) {
+              //verifica se o numero pode ser colocado
+              if(torres[linha+1][coluna-1] > numeroPMover) {
+                torres[localizacaoDoNum[0]][localizacaoDoNum[1]] = 0;
+                torres[linha][coluna-1] = numeroPMover;
+
+                System.out.println("numero movido");
+                Torres.MostraMatriz(torres);
+              } else {
+                Torres.pulaLinha();
+                Torres.divisor();
+                System.out.println("Um número maior nunca pode ser colocado sobre um número menor.");
+                Torres.divisor();
+              }
+            } else {
               torres[localizacaoDoNum[0]][localizacaoDoNum[1]] = 0;
               torres[linha][coluna-1] = numeroPMover;
-
               System.out.println("numero movido");
-              MostraMatriz(torres);
-            } else {
-              pulaLinha();
-              divisor();
-              System.out.println("Um número maior nunca pode ser colocado sobre um número menor.");
-              divisor();
+              Torres.MostraMatriz(torres);
             }
-          } else {
-            torres[localizacaoDoNum[0]][localizacaoDoNum[1]] = 0;
-            torres[linha][coluna-1] = numeroPMover;
-            System.out.println("numero movido");
-            MostraMatriz(torres);
+          } 
+
+          jogadas++;
+
+          if (Torres.compararMatrizes(torres, possivelResultado1, possivelResultado2)) {
+            Torres.pulaLinha();
+            Torres.divisor();
+            System.out.println("Parabéns, Você completou o jogo em " + jogadas + " jogadas!");
+            Torres.divisor();
+            value = 10;
           }
-        } 
-
-        jogadas++;
-
-        if (compararMatrizes(torres, possivelResultado1, possivelResultado2)) {
-          pulaLinha();
-          divisor();
-          System.out.println("Parabéns, Você completou o jogo em " + jogadas + " jogadas!");
-          divisor();
-          value = 10;
         }
-      }
-    }while (value != 10);
+      }while (value != -1);
+
+    inp.close();
   }
 }
--- Torres.java
+++ Torres.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class Torres {
+
+  public static Scanner inp = new Scanner(System.in);
+
   public static void main(String[] args) {
-    Scanner inp = new Scanner(System.in);
 
     pulaLinha();
     pulaLinha();
@@ -18,109 +20,18 @@ public class Torres {
 
     Comece o jogo:
     """);
-    System.out.println("quantos números voce deseja usar? (quanto mais números, mais demorado!)");
-    int qntDiscos = inp.nextInt();
-  
-    int [] []torres = new int[qntDiscos+1] [3];
-    int [] []possivelResultado1 = new int[qntDiscos+1] [3];
-    int [] []possivelResultado2 = new int[qntDiscos+1] [3];
+    
+    System.out.println("""
+        MODOS DE JOGO:
 
-    pulaLinha();
-    System.out.println("Sua piramide tem " + qntDiscos + " números!");
+        1. MANUAL
+        2. AUTOMATICO
+        """);
+    int modo = inp.nextInt();
 
-    int cont = 0;
-
-    //inicializando as torres
-    for (int i = 0; i < torres.length; i++) {
-        torres[i][0] = cont;
-        possivelResultado1[i][1] = cont;
-        possivelResultado2[i][2] = cont;
-        cont++;
+    if(modo == 1) {
+      Manual.modoManual();
     }
-
-    MostraMatriz(torres);
-    pulaLinha();
-
-    int value = 0;
-    int jogadas = 0;
-    do {
-        System.out.println("você pode desistir a qualquer momento digitando 10");
-        System.out.println("digite o valor que deseja mover: ");
-
-        int numeroPMover = inp.nextInt();
-        value = numeroPMover;
-
-        //verifica se o valor digitado é zero
-        if (numeroPMover == 0){
-          divisor();
-          System.out.println("O valor zero indica que o espaço está vazio!");
-          divisor();
-        }
-        int [] localizacaoDoNum = localizaPosicao(torres, numeroPMover);
-        
-        //verifica se o valor digitado existe na torre
-        if(localizacaoDoNum == null) {
-          divisor();
-          System.out.println("O numero digitado não existe na torre!");
-          divisor();
-        }
-        else if(torres[localizacaoDoNum[0]-1][localizacaoDoNum[1]] > 0) {
-          divisor();
-          System.out.println("você só pode mover o numero do topo de uma coluna! ");
-          divisor();
-        }
-        else {
-          System.out.println("digite a coluna para onde deseja mover o numero");
-          int coluna = inp.nextInt();
-          value = coluna;
-          //verifica se a coluna existe
-          if (coluna > 3 || coluna < 1) {
-            divisor();
-            System.out.println("numero invalido, você só tem 3 colunas");
-            divisor();
-          } else{
-            int linha = qntDiscos;
-
-            //verifica se o numero a ser movido pode ser colocado na torre
-            if (torres[linha][coluna-1] != 0 ){
-              while (torres[linha][coluna-1] != 0 ) {
-                linha--;
-              }
-
-              //verifica se o numero pode ser colocado
-              if(torres[linha+1][coluna-1] > numeroPMover) {
-                torres[localizacaoDoNum[0]][localizacaoDoNum[1]] = 0;
-                torres[linha][coluna-1] = numeroPMover;
-
-                System.out.println("numero movido");
-                MostraMatriz(torres);
-              } else {
-                pulaLinha();
-                divisor();
-                System.out.println("Um número maior nunca pode ser colocado sobre um número menor.");
-                divisor();
-              }
-            } else {
-              torres[localizacaoDoNum[0]][localizacaoDoNum[1]] = 0;
-              torres[linha][coluna-1] = numeroPMover;
-              System.out.println("numero movido");
-              MostraMatriz(torres);
-            }
-          } 
-
-          jogadas++;
-
-          if (compararMatrizes(torres, possivelResultado1, possivelResultado2)) {
-            pulaLinha();
-            divisor();
-            System.out.println("Parabéns, Você completou o jogo em " + jogadas + " jogadas!");
-            divisor();
-            value = 10;
-          }
-        }
-      }while (value != 10);
-
-    inp.close();
   }
 
   public static void MostraMatriz(int [][]torres) {
